f2e09d993b3c96ff19b62b4dbc08ac809c019333
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-30 09:11:38 -0300

    att icons dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { LoginComponent } from './shared/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path:'',
+    component: LoginComponent
+  },
   {
     path:'departamento',
     loadChildren: () => import('./departamento/departamento.module').then((d) => d.DepartamentoModule)

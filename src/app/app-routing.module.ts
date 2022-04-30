5b699eb0987762397ea48492fe1860a5be792eb2
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-04-30 16:24:29 -0300

    create requisicao component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,19 @@ const routes: Routes = [
   {
     path:'departamento',
     loadChildren: () => import('./departamento/departamento.module').then((d) => d.DepartamentoModule)
-  }
+  },
+  {
+    path:'funcionario',
+    loadChildren: () => import('./funcionario/funcionario.module').then((f) => f.FuncionarioModule)
+  },
+  {
+    path:'requisicao',
+    loadChildren: () => import('./requisicao/requisicao.module').then((r) => r.RequisicaoModule)
+  },
+  {
+    path:'movimentacao',
+    loadChildren: () => import('./movimentacao/movimentacao.module').then((m) => m.MovimentacaoModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/requisicao/requisicao.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requisicao',
+  templateUrl: './requisicao.component.html',
+  styleUrls: ['./requisicao.component.sass']
+})
+export class RequisicaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  add(){}
+
+  delete(){}
+
+  selecionaRequisicao(){
+
+  }
+}
